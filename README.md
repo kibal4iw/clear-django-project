b9d4d22616c3847beae3f8ae6716fc2f51648d6e
A. Panteleev <mak@MacBook-Pro-mak.local>
2021-07-06 00:37:06 +0300

    end lessons 9

--- README.md
+++ README.md
@@ -20,6 +20,24 @@ python3 manage.py makemigrations
 
 python3 manage.py migrate
 
+#### Установка thumblains для проекта
+pip install sorl-thumbnail
+
+python manage.py migrate
+
+в шаблоне:
+`
+{% load thumbnail %}
+{% thumbnail image.image "300" as im %}
+    <a href="{{ image.image.url }}">
+        <img src="{{ im.url }}" class="image-detail">
+    </a>
+{% endthumbnail %}
+
+источник https://pocoz.gitbooks.io/django-v-primerah/content/glava-5-obschii-dostup-k-kontentu-na-saite/sozdanie-miniatyur-izobrazhenii-s-pomoschyu-sorl-thumbnail.html
+`
+
+
 ### Источник
 https://www.haikson.com/programmirovanie/python/django-nginx-gunicorn-postgresql-docker/ 
 
--- project/movies/views.py
+++ project/movies/views.py
@@ -1,21 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
 from .models import Movie
+from .forms import ReviewForm
 
 
 class MoviesView(ListView):
+    """ Список фильмов """
     model = Movie
     queryset = Movie.objects.filter(draft=False)
     # template_name = "movies/movies.html"
 
 
 class MoviesDetailsVies(DetailView):
+    """ Детали фильма """
     model = Movie
     slug_field = "url"
     # template_name = "movies/movie_detail.html"
 
+
+class AddReview(View):
+    """ Добавление отзыва """
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+
+        if form.is_valid():
+            form = form.save(commit=False)
+            # form.movie_id = pk
+            form.movie = movie
+            form.save()
+
+        return redirect(movie.get_absolute_url())
 
 # Create your views here.
 #class MoviesView(View):
