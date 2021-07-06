0e27934575ee1430d39ce5e0c63f2b13ea42af4c
A. Panteleev <mak@MacBook-Pro-mak.local>
2021-07-06 20:14:50 +0300

    end lessons 10

--- project/movies/views.py
+++ project/movies/views.py
@@ -29,6 +29,8 @@ class AddReview(View):
         if form.is_valid():
             form = form.save(commit=False)
             # form.movie_id = pk
+            if request.POST.get("parent", None):
+                form.parent_id = int(request.POST.get("parent"))
             form.movie = movie
             form.save()
 
